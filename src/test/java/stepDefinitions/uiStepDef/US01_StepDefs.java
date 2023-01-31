1bda46d351bbfca9f525fe61eba4eeca8b1da12d
Vural CoLak <vuralcolakcontact@gmail.com>
2023-01-31 20:51:02 +0100

    31.01.23 Vural My first Main method

--- src/test/java/stepDefinitions/uiStepDef/US01_StepDefs.java
+++ src/test/java/stepDefinitions/uiStepDef/US01_StepDefs.java
@@ -1,6 +1,7 @@
 package stepDefinitions.uiStepDef;
 
 public class US01_StepDefs {
-    // Burasi coftlict alani
-    //Bu ilk step defs
+    public static void main(String[] args) {
+        System.out.println("I passed here, Vural");
+    }
 }
