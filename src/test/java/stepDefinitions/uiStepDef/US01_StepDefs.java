48f68d9998db3341a2adba0a776a642393a600a0
Nesibe <nesibekrn@gmail.com>
2023-01-31 22:56:38 +0300

    31.01.23 Nesibe new conflict

--- src/test/java/stepDefinitions/uiStepDef/US01_StepDefs.java
+++ src/test/java/stepDefinitions/uiStepDef/US01_StepDefs.java
@@ -2,6 +2,7 @@ package stepDefinitions.uiStepDef;
 
 public class US01_StepDefs {
     public static void main(String[] args) {
+        System.out.println("I passed too, Nesibe");
         System.out.println("I passed here, Vural");
     }
 }
