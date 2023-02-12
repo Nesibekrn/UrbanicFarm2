b0bfc78621fbe87982bcd087b24e0cd6aa23f4d3
Rumeysa Baloglu <rumeysabal35@gmail.com>
2023-02-12 00:18:33 -0800

    02/12/23 Rumeysa US025 US044 added

---
+++ src/test/java/stepDefinitions/uiStepDef/US044_StepDef.java
@@ -0,0 +1,15 @@
+package stepDefinitions.uiStepDef;
+
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pages.CommonPage;
+
+public class US044_StepDef extends CommonPage {
+
+    @Then("User verifies logout botton is clickable")
+    public void userVerifiesLogoutBottonIsClickable() {
+        Assert.assertTrue(getHomePage().logoutButton2.isEnabled());
+        getHomePage().logoutButton2.click();
+    }
+}
+
