54730943c8af3f35d0dbd5d9a6f0540321f7981d
Shukhrat CHORSHANBIEV <shregarinho@gmail.com>
2023-03-11 00:00:04 +0300

    Version problem resolved

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@US099"       
+        tags = "@US077_TC_02"
 
 
 )
