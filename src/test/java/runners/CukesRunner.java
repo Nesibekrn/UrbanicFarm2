2b1cb3d62fb40eb628322ea50a2400bdf79bf7e0
Rumeysa Baloglu <rumeysabal35@gmail.com>
2023-03-15 23:27:10 -0700

    Rumeysa US105, US106 03/15/2023

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@US085"
+        tags = "@US106"
 )
 public class CukesRunner {
 }
