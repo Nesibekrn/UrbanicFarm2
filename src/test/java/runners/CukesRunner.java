4904bf83f256ee3554fc9ec27c718406231adbeb
aytenmarul <58526062+aytenmarul@users.noreply.github.com>
2023-03-15 22:01:47 -0700

    091 was extra class so it is deleted

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@US080"
+        tags = "@UF2-331"
 )
 public class CukesRunner {
 }
